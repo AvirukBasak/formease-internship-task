5c0046d6b379e85134907cac1dfe8d9948777e1f
AvirukBasak <avirukbasak@gmail.com>
2023-12-06 19:47:58 +0530

    built switch

---
+++ src/pages/components/Switch.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+type SwitchProps = {
+  onLabel?: string;
+  offLabel?: string;
+  defState?: boolean;
+};
+
+export default function Switch({ onLabel = "Yes", offLabel = "No", defState = false }: SwitchProps) {
+  const [toggle, setToggle] = useState(!defState);
+  const toggleClassBg = " transform bg-green-500";
+  const toggleClassFg = " transform translate-x-2 bg-white";
+  return (
+    <div
+      className={
+        "text-white w-16 h-8 pl-1 pr-1 flex items-center rounded-full cursor-pointer " +
+        (toggle ? "bg-red-400" : toggleClassBg)
+      }
+      onClick={() => {
+        setToggle(!toggle);
+      }}>
+
+      {!toggle ? <span className="text-sm ml-1">{onLabel}</span> : ""}
+
+      <div
+        className={
+          "h-6 w-6 rounded-full transform duration-200 ease-in-out " +
+          (toggle ? "bg-red-600" : toggleClassFg)
+        }
+      ></div>
+
+      {toggle ? <span className="text-sm ml-1">{offLabel}</span> : ""}
+
+    </div>
+  );
+}
