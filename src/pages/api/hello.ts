829c696c5849d46ad0ab43043116aa34b946ea4c
AvirukBasak <avirukbasak@gmail.com>
2023-12-06 18:36:24 +0530

    init

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -8,6 +7,6 @@ type Data = {
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
-) {
+): void {
   res.status(200).json({ name: 'John Doe' })
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import React from 'react'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
