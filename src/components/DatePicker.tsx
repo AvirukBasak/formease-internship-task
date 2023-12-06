602a5f1a447bc6a5e95591eb4d909bbd88acd636
AvirukBasak <avirukbasak@gmail.com>
2023-12-06 22:33:51 +0530

    date picker done

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+
 interface DatePickerProps {
   onDateSet?: (date: Date) => void
 }
 
 export default function DatePicker({ onDateSet }: DatePickerProps): JSX.Element {
-    return <></>
+  const [date, setDate] = useState<Date | null>(null);
+  return (
+    <input
+      className="
+          md:w-56
+          rounded-md border border-gray-300
+          px-3 pt-3 pb-4
+          bg-white
+          text-sm text-gray-700
+          hover:bg-gray-50
+          focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100"
+      type="date"
+      value={date?.toISOString().split("T")[0] || ""}
+      onChange={(e) => setDate(new Date(e.target.value))}
+    />
+  )
 }
