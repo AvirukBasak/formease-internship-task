cf8db6127bb9a52ec95ba5d92884fd906bdca98c
AvirukBasak <avirukbasak@gmail.com>
2023-12-07 14:22:14 +0530

    toggle switch bug fix

--- src/components/ToggleSwitch.tsx
+++ src/components/ToggleSwitch.tsx
@@ -4,34 +4,32 @@ interface ToggleSwitchProps {
   onLabel?: string;
   offLabel?: string;
   defState?: boolean;
-  onStateChange?: (state: boolean) => void;
+  onStateChange?: (newState: boolean) => void;
 };
 
 export default function ToggleSwitch({ onLabel = "Yes", offLabel = "No", defState = false, onStateChange }: ToggleSwitchProps): JSX.Element {
-  const [toggle, setToggle] = useState(!defState);
-  const toggleClassBg = " transform bg-green-400";
-  const toggleClassFg = " transform translate-x-2 bg-white";
+  const [toggle, setToggle] = useState(defState);
   return (
     <div
       className={
         "text-white w-16 h-8 pl-1 pr-1 flex items-center rounded-full cursor-pointer " +
-        (toggle ? "bg-red-400" : toggleClassBg)
+        (toggle ? "transform bg-green-400" : "bg-red-400")
       }
       onClick={() => {
         setToggle(!toggle);
         onStateChange && onStateChange(!toggle);
       }}>
 
-      {!toggle && <span className="text-sm ml-1">{onLabel}</span>}
+      {toggle && <span className="text-sm ml-1">{onLabel}</span>}
 
       <div
         className={
           "h-6 w-6 rounded-full transform duration-200 ease-in-out " +
-          (toggle ? "bg-red-600" : toggleClassFg)
+          (toggle ? "transform translate-x-2 bg-white" : "bg-red-600")
         }
       />
 
-      {toggle && <span className="text-sm ml-1">{offLabel}</span>}
+      {!toggle && <span className="text-sm ml-1">{offLabel}</span>}
 
     </div>
   );
