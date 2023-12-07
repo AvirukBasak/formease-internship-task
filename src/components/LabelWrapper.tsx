dbcd85986e9f65ee5ac456bf66c9808b573f240d
AvirukBasak <avirukbasak@gmail.com>
2023-12-07 15:43:59 +0530

    updated PartyInfo page

--- src/components/LabelWrapper.tsx
+++ src/components/LabelWrapper.tsx
@@ -5,11 +5,15 @@ interface LaelWrapperProps {
   width?: string
 }
 
-export default function LabelWrapper({ label = "&nbsp;", children, showLabel = true, width = "w-56" }: LaelWrapperProps): JSX.Element {
+export default function LabelWrapper({ label, children, showLabel = true, width = "w-56" }: LaelWrapperProps): JSX.Element {
   return (
     <>
       <div className={width}>
-        {showLabel && <label className="mr-auto text-sm font-thin text-black block">{label}</label>}
+        {
+          showLabel && <label className="mr-auto mb-1 text-sm font-thin text-black block">
+            {label || <span>&nbsp;&nbsp;</span>}
+          </label>
+        }
         {children}
       </div>
     </>
