3acce3fbae78b4f5b55ec5d7dfcaee3ed9a64220
AvirukBasak <avirukbasak@gmail.com>
2023-12-07 13:33:29 +0530

    update navigator

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -8,30 +8,38 @@ interface NavigatorProps {
 export default function Navigator({ labels, pages }: NavigatorProps): JSX.Element {
   const [currentPage, setCurrentPage] = useState(0);
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-row justify-center">
+    <div className="md:w-full flex flex-col items-center">
+      <div className="flex flex-row justify-evenly w-full">
+        <div className="h-0.5 bg-gray-800 absolute top-20 -z-10" style={{ width: "60%" }} />
         {labels.map((label, index) => {
           return (
             <div
               key={index}
-              className="flex flex-col items-center justify-center"
+              className="flex flex-col items-center justify-center cursor-pointer"
               onClick={() => setCurrentPage(index)}
             >
-              <div
-                className={`
-                  flex flex-col items-center justify-center
-                  rounded-full border-4 border-dodgerBlue
-                  w-10 h-10 ${index === currentPage ? "bg-dodgerBlue" : "bg-white"}`
-                }
+              <div className={`
+                mb-2 w-20 h-20
+                flex flex-col items-center justify-center
+                rounded-full border-2
+                ${index === currentPage ? "bg-white border-blue-800" : "bg-gray-300 border-gray-700"}`}
               >
-                <div className="text-white font-bold">{index + 1}</div>
+                <div
+                  className={`text-xl ${index === currentPage ? " text-blue-800" : " text-gray-800"}`}
+                >
+                  {index + 1}
+                </div>
               </div>
-              <div className="text-dodgerBlue">{label}</div>
+              <div
+                className={` text-sm ${index === currentPage ? " text-blue-800" : " text-gray-800"}`}
+              >
+                {label}
+              </div>
             </div>
           )
         })}
       </div>
-      <div className="flex flex-row justify-center">
+      <div className="md:w-full flex flex-row justify-center">
         {pages.map((page, index) => {
           return (
             <div key={index} className={`${index === currentPage ? "" : "hidden"}`}>
